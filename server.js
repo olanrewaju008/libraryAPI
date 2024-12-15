423407c26b9a84427064ea044fa5c4f0bdf8f5c0
olanrewajue <olanrewajuhammed008@gmail.com>
2024-12-15 17:08:48 +0100

    server update and user router set up

--- server.js
+++ server.js
@@ -7,8 +7,16 @@ const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 
+const bookRouter = require('./router/bookRouter')
+const libraryRouter = require('./router/libraryRouter')
+const userRouter = require('./router/userRouter')
+
 const url = process.env.DATABASE_URL;
 const port = process.env.port || 2025;
+
+app.use('/api/v1', bookRouter)
+app.use('/api/v1',libraryRouter)
+app.use('api/v1', userRouter)
 
 app.get("/", (req, res) => {
   res.send("Welcome to Book shop");
